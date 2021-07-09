84960c7f412523910fcedb5bac2670a8ef4b0cd6
Sun Seung-hoon <sunsh@uangel.com>
2021-07-09 15:28:06 +0900

    3장 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+//      <Food fav="kimchi" something={true} papapapa={['hello', 1, 2, 3, 4, true]} />
+
+//App 컴포넌트 정의
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello !!</h1>
+      <Potato />
+      <Food fav="kimchi"  />
     </div>
   );
 }
 
+function Potato() {
+  return <h3>i love potato</h3>
+}
+
+
+function Food1({ fav }) {
+  return <div> {fav}</div>;
+}
+
+function Food(props) {
+  console.log(props);
+  return (
+    <div>
+      <h4>food food</h4>
+      <h4>{ props.fav }</h4>
+      <h4>{ props.papapapa }</h4>
+    </div>
+  );
+}
 export default App;
